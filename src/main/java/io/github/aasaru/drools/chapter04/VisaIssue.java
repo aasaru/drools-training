a15eb16475aedeb33454fac683922bcf1e58184e
Juhan Aasaru <aasaru@gmail.com>
2018-10-09 11:24:22 +0300

    turn step1 of chapter02 into individual chapter

---
+++ src/main/java/io/github/aasaru/drools/chapter04/VisaIssue.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2017 Juhan Aasaru.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.aasaru.drools.chapter04;
+
+import io.github.aasaru.drools.Common;
+import io.github.aasaru.drools.domain.Passport;
+import io.github.aasaru.drools.domain.Visa;
+import io.github.aasaru.drools.domain.VisaApplication;
+import io.github.aasaru.drools.repository.ApplicationRepository;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.Agenda;
+
+import java.util.Collection;
+import java.util.List;
+
+public class VisaIssue {
+  public static void main(final String[] args) {
+    execute(Common.promptForStep(3, args, 1, 3));
+  }
+
+
+  static void execute(int step) {
+    System.out.println("Running step " + step);
+    KieSession ksession = KieServices.Factory.get().getKieClasspathContainer().newKieSession("VisaIssueStep" + step);
+
+    List<Passport> passports = ApplicationRepository.getPassports();
+    passports.forEach(ksession::insert);
+
+    List<VisaApplication> visaApplications = ApplicationRepository.getVisaApplications();
+    visaApplications.forEach(ksession::insert);
+
+    if (step == 3) {
+      Agenda agenda = ksession.getAgenda();
+      agenda.getAgendaGroup("issue-visa").setFocus();
+      agenda.getAgendaGroup("valid-application").setFocus();
+      agenda.getAgendaGroup("invalid-application").setFocus();
+      agenda.getAgendaGroup("valid-passport").setFocus();
+      agenda.getAgendaGroup("invalid-passport").setFocus();
+    }
+
+    System.out.println("==== DROOLS SESSION START ==== ");
+    ksession.fireAllRules();
+    ksession.dispose();
+    System.out.println("==== DROOLS SESSION END ==== ");
+
+
+    System.out.println("==== APPLICATIONS STATE AFTER DROOLS SESSION === ");
+    visaApplications.forEach(visaApplication -> System.out.println(visaApplication + " verdict: " + visaApplication.getValidation()));
+
+
+//    Collection<?> ksessionObjects = ksession.getObjects();
+//    System.out.println("== All objects from session == ");
+//    ksessionObjects.forEach(System.out::println);
+
+
+    Collection<?> visaObjects = ksession.getObjects(o -> o.getClass() == Visa.class);
+    System.out.println("== Visas from session == ");
+    visaObjects.forEach(System.out::println);
+
+
+  }
+
+}
--- src/main/java/io/github/aasaru/drools/domain/Passport.java
+++ src/main/java/io/github/aasaru/drools/domain/Passport.java
@@ -20,101 +20,111 @@ package io.github.aasaru.drools.domain;
 import java.time.LocalDate;
 
 public class Passport {
-  private String passportNumber;
-  private String name;
-  private LocalDate expiresOn;
-  private int unusedVisaPages;
-  private boolean underage;
-
-  private Validation validation = Validation.UNKNOWN;
-
-  private Passport() {
-  }
-
-  public LocalDate getExpiresOn() {
-    return expiresOn;
-  }
-
-  public boolean isExpired() {
-    return expiresOn.isBefore(LocalDate.now());
-  }
-
-  public String getPassportNumber() {
-    return passportNumber;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public int getUnusedVisaPages() {
-    return unusedVisaPages;
-  }
-
-  public Validation getValidation() {
-    return validation;
-  }
-
-  public void setValidation(Validation validation) {
-    this.validation = validation;
-  }
-
-  public boolean isUnderage() {
-    return underage;
-  }
-
-  @Override
-  public String toString() {
-    return String.format("Passport[no:%s, name:%s]", passportNumber, name);
-  }
-
-  public static PassportBuilder newBuilder() {
-    return new PassportBuilder();
-  }
-
-  public static final class PassportBuilder {
     private String passportNumber;
     private String name;
     private LocalDate expiresOn;
     private int unusedVisaPages;
     private boolean underage;
 
-    private PassportBuilder() {
+    private Validation validation = Validation.UNKNOWN;
+
+    private String cause = "";
+
+    private Passport() {
     }
 
-    public PassportBuilder withPassportNumber(String passportNumber) {
-      this.passportNumber = passportNumber;
-      return this;
+    public LocalDate getExpiresOn() {
+        return expiresOn;
     }
 
-    public PassportBuilder withName(String name) {
-      this.name = name;
-      return this;
+    public boolean isExpired() {
+        return expiresOn.isBefore(LocalDate.now());
     }
 
-    public PassportBuilder withExpiresOn(LocalDate expiresOn) {
-      this.expiresOn = expiresOn;
-      return this;
+    public String getPassportNumber() {
+        return passportNumber;
     }
 
-    public PassportBuilder withUnusedVisaPages(int unusedVisaPages) {
-      this.unusedVisaPages = unusedVisaPages;
-      return this;
+    public String getName() {
+        return name;
     }
 
-    public PassportBuilder withUnderage(boolean underage) {
-      this.underage = underage;
-      return this;
+    public int getUnusedVisaPages() {
+        return unusedVisaPages;
     }
 
-    public Passport build() {
-      Passport passport = new Passport();
-      passport.passportNumber = passportNumber;
-      passport.name = name;
-      passport.expiresOn = expiresOn;
-      passport.unusedVisaPages = unusedVisaPages;
-      passport.underage = underage;
-      return passport;
+    public Validation getValidation() {
+        return validation;
     }
-  }
+
+    public void setValidation(Validation validation) {
+        this.validation = validation;
+    }
+
+    public boolean isUnderage() {
+        return underage;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Passport[no:%s, name:%s]", passportNumber, name);
+    }
+
+    public static PassportBuilder newBuilder() {
+        return new PassportBuilder();
+    }
+
+    public static final class PassportBuilder {
+        private String passportNumber;
+        private String name;
+        private LocalDate expiresOn;
+        private int unusedVisaPages;
+        private boolean underage;
+
+        private PassportBuilder() {
+        }
+
+        public PassportBuilder withPassportNumber(String passportNumber) {
+            this.passportNumber = passportNumber;
+            return this;
+        }
+
+        public PassportBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PassportBuilder withExpiresOn(LocalDate expiresOn) {
+            this.expiresOn = expiresOn;
+            return this;
+        }
+
+        public PassportBuilder withUnusedVisaPages(int unusedVisaPages) {
+            this.unusedVisaPages = unusedVisaPages;
+            return this;
+        }
+
+        public PassportBuilder withUnderage(boolean underage) {
+            this.underage = underage;
+            return this;
+        }
+
+        public Passport build() {
+            Passport passport = new Passport();
+            passport.passportNumber = passportNumber;
+            passport.name = name;
+            passport.expiresOn = expiresOn;
+            passport.unusedVisaPages = unusedVisaPages;
+            passport.underage = underage;
+            return passport;
+        }
+    }
 }
