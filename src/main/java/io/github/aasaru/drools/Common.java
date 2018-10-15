45c9b2f34a89d60e342b7a174103e8de995cf0e4
Juhan Aasaru <aasaru@gmail.com>
2018-10-15 13:36:21 +0300

    change license

--- src/main/java/io/github/aasaru/drools/Common.java
+++ src/main/java/io/github/aasaru/drools/Common.java
@@ -1,18 +1,11 @@
 /*
- * Copyright 2017 Juhan Aasaru.
+ *  Drools Online Course Sample Code and Study Materials (c) by Juhan Aasaru.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *  Drools Online Course Sample Code and Study Materials is licensed under a
+ *  Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the license along with this
+ *  work. If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
  */
 
 package io.github.aasaru.drools;
