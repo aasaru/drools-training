586131a998520e6de17016d0cb2a9257efad3261
Juhan Aasaru <aasaru@gmail.com>
2023-12-01 15:55:16 +0200

    add license

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 This project is part of Drools video course 
 [Master Drools Programming—Learn How to Write Drools Rules](https://www.udemy.com/course/master-drools/?referralCode=ED7C311E09498C940742 "Click to go to Udemy course")
 
-The instructions how to run this code are given in the course.
+The instructions for how to run this code are given in the course.
--- src/main/java/io/github/aasaru/droolsoopath/section08/step1/RuleUnitSection08.java
+++ src/main/java/io/github/aasaru/droolsoopath/section08/step1/RuleUnitSection08.java
@@ -1,3 +1,13 @@
+/*
+ *  Drools Online Course Sample Code and Study Materials (c) by Juhan Aasaru.
+ *
+ *  Drools Online Course Sample Code and Study Materials is licensed under a
+ *  Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ *
+ *  You should have received a copy of the license along with this work.
+ *  If not, see <http://creativecommons.org/licenses/by-nc-nd/4.0/>.
+ */
+
 package io.github.aasaru.droolsoopath.section08.step1;
 
 public class RuleUnitSection08 extends io.github.aasaru.droolsoopath.section08.RuleUnitSection08 {
